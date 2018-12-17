2a4193d2d47b5424ef50b9ecee8dad9cdbabd2ee
siria <diansiria@gmail.com>
2018-12-17 20:20:29 +0100

    update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Listen for messages
 browser.runtime.onMessage.addListener(receiver);
+const body = document.querySelector("body");
 
 // A message is received
 function receiver(request, sender, sendResponse) {
   if (request.active) {
     //do something
+    body.classList.toggle("my-beautiful-extension");
   } else {
     //do else
+    body.classList.toggle("my-beautiful-extension");
   }
 }
